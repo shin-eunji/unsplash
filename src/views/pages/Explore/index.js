48da5173acb9a14827850d90ea4e7e0b597ee7a5
shineunji <dmswlckato@gmail.com>
2020-11-16 19:58:32 +0900

    jwt

--- src/views/pages/Explore/index.js
+++ src/views/pages/Explore/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import Title from "../../components/Text/Title/Title";
 
 import {pxToRem} from "../../../common/Text/Text.Styled";
-import {Index, subTitle} from "../../components/Text/Title";
 import {ContentContainer} from "../../../common/Layout/Components.Styled";
 import {category} from "../../components/Category/CategoryList";
 
-import SubTitle from "../../components/Text/Title/SubTitle";
 import Category from "../../components/Category/Category";
 
 function Explore(props) {
@@ -17,12 +14,16 @@ function Explore(props) {
     return (
         <Container>
             <SContentContainer>
-                <Title data={Index.explore}/>
-
-                <SubTitle data={subTitle.browse}/>
-                <Category data={category}/>
-
-                <SubTitle data={subTitle.free}/>
+                <Text>
+                    <Thumbnail>Explore</Thumbnail>
+                    <Title>Explore</Title>
+                    <Description>Unsplash has over a million free high-resolution photos. Explore these popular photo categories on Unsplash. All photos here are free to download and use under the<p>Unsplash License.</p></Description>
+                </Text>
+                <Contents>
+                    <SubTitle>Browse thousands of free images on Unsplash</SubTitle>
+                    <Category data={category}/>
+                </Contents>
+                <SubTitle>Free high-resolution Unsplash photos</SubTitle>
             </SContentContainer>
         </Container>
     )
@@ -30,15 +31,42 @@ function Explore(props) {
 
 const Container = styled.div`
 `
-const Header = styled.div`
-    position:fixed;
-    top: 0;
-    left: 0;
-    display:flex;
-    flex-direction:column;
-    width: 100%;
-    background: #fff;
-    z-index: 100;
+const Text = styled.div`
+    padding: ${pxToRem(60)} 0 ${pxToRem(72)};
+    color: #111;
+`;
+const Title = styled.h2`
+    font-size: ${pxToRem(46)};
+    line-height: 1.2;
+    font-weight: 700;
+    margin-bottom: ${pxToRem(16)};
+`;
+const Thumbnail = styled.div`
+    font-size: ${pxToRem(16)};
+    line-height: 1.2;
+    font-weight: 400;
+`;
+const Description = styled.div`
+    font-size: ${pxToRem(18)};
+    font-weight: 400;
+    line-height: 1.6;
+    p {
+        text-decoration: underline;
+        color: #767676;
+        cursor: pointer;
+        &:hover {
+            color: #111;
+        }
+    }
+`;
+const Contents = styled.div`
+    
+`;
+const SubTitle = styled.div`
+    color: #111;
+    font-size: ${pxToRem(28)};
+    font-weight: 700;
+    margin-bottom: ${pxToRem(32)};
 `;
 const SContentContainer = styled(ContentContainer)`
     position:relative;
